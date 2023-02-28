52589b8211d04b5eabae72d9ea4587941dd05931
Valeria D'Amante <46978288+valeriadamante@users.noreply.github.com>
2023-02-28 15:18:25 +0100

    Revert "implementation of btag sf"

--- Common/Utilities.py
+++ Common/Utilities.py
@@ -1,14 +1,12 @@
-from enum import Enum
 import ROOT
-import copy
 
-class WorkingPointsTauVSmu(Enum):
+class WorkingPointsTauVSmu:
     VLoose = 1
     Loose = 2
     Medium = 3
     Tight = 4
 
-class WorkingPointsTauVSjet(Enum):
+class WorkingPointsTauVSjet:
    VVVLoose = 1
    VVLoose = 2
    VLoose = 3
@@ -18,7 +16,7 @@ class WorkingPointsTauVSjet(Enum):
    VTight = 7
    VVTight = 8
 
-class WorkingPointsTauVSe(Enum):
+class WorkingPointsTauVSe:
     VVVLoose = 1
     VVLoose = 2
     VLoose = 3
@@ -28,7 +26,7 @@ class WorkingPointsTauVSe(Enum):
     VTight = 7
     VVTight = 8
 
-class WorkingPointsBoostedTauVSjet(Enum):
+class WorkingPointsBoostedTauVSjet:
    VVLoose = 1
    VLoose = 2
    Loose = 3
@@ -36,11 +34,6 @@ class WorkingPointsBoostedTauVSjet(Enum):
    Tight = 5
    VTight = 6
    VVTight = 7
-
-class WorkingPointsbTag(Enum):
-    Loose = 1
-    Medium = 2
-    Tight = 3
 
 def ListToVector(list, type="string"):
 	vec = ROOT.std.vector(type)()
@@ -50,29 +43,9 @@ def ListToVector(list, type="string"):
 
 def generate_enum_class(cls):
     enum_string = "enum class {} : int {{\n".format(cls.__name__)
-    for item in cls:
-        enum_string += "    {} = {},\n".format(item.name, item.value)
+    for name, value in cls.__dict__.items():
+        if not name.startswith("__"):
+            enum_string += "    {} = {},\n".format(name, value)
     enum_string += "};"
     return enum_string
 
-class DataFrameWrapper:
-    def __init__(self, df, defaultColToSave=[]):
-        self.df = df
-        self.colToSave = copy.deepcopy(defaultColToSave)
-
-    def Define(self, varToDefine, varToCall):
-        self.df = self.df.Define(f"{varToDefine}", f"{varToCall}")
-
-    def DefineAndAppend(self, varToDefine, varToCall):
-        self.Define(varToDefine, varToCall)
-        self.colToSave.append(varToDefine)
-
-    def Apply(self, func, *args, **kwargs):
-        result = func(self.df, *args, **kwargs)
-        if isinstance(result, tuple):
-            self.df = result[0]
-            if len(result) == 2:
-                return result[1]
-            return result[1:]
-        else:
-            self.df = result
